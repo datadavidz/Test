49ac2229a2a891c3e01bd27236e29701fc0b90d4
David Zoller <david.l.zoller@gmail.com>
2016-08-16 21:14:23 -0500

    Load the long FR data file

--- Test.R
+++ Test.R
@@ -1,1 +1,16 @@
 # this is a test script
+
+readSpotfire <- function(filename, file_dir) {
+  setwd(file_dir)
+  
+  # the UTF-16 encoding is required or it won't import
+  # for the FR data it wouldn't load with quote disabled
+  df <- read.table(filename, sep="\t", fileEncoding="UTF-16", comment.char="",
+                   header=TRUE, stringsAsFactors=FALSE)
+  return(df)
+}
+
+filename = "ALL UL-VX5 LEXAN NO PIVOT.txt"
+file_dir = "c:/Users/David Zoller/Documents/_New/FR"
+
+fr_data <- readSpotfire(filename, file_dir)
